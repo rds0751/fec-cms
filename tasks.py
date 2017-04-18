71216a4d10a4c0cbe77b06fe7105d766e2837d84
LindsayYoung <lindsayn.young@gsa.gov>
2017-04-18 14:57:33 -0400

    deploy

--- tasks.py
+++ tasks.py
@@ -75,7 +75,7 @@ DEPLOY_RULES = (
     # ('prod', _detect_prod),
     # ('stage', lambda _, branch: branch.startswith('release')),
     # ('dev', lambda _, branch: branch == 'develop'),
-    ('stage', lambda _, branch: branch == 'develop'),
+    ('stage', lambda _, branch: branch == 'deploy-quick'),
 )
 
 
