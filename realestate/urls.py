b194b99509edead033c84c9cc73a3038b18d7ce1
Ripudaman Singh <rds0751@gmail.com>
2019-11-25 11:04:18 +0530

    added eb package configuration

---
+++ realestate/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+path('', include('pages.urls')),
+path('listings/', include('listings.urls')),
+path('accounts/', include('accounts.urls')),
+# path('', include('contacts.urls')),
+path('admin/', admin.site.urls),
+path('reviews/', include('reviews.urls')),
+
+path('blog/', include(('blog.urls', 'blog'), namespace='blog')),
+path('ckeditor/', include('ckeditor_uploader.urls')),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
