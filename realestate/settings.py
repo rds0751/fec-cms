0d2d60c826697daeca6060542538ba5b9564b00a
Ripudaman Singh <rds0751@gmail.com>
2019-12-06 16:21:17 +0530

    msg

--- realestate/settings.py
+++ realestate/settings.py
@@ -81,7 +81,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'realestate.urls'
