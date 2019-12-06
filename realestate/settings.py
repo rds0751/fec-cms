f12f7a945d9ec2eb5e5bf549e157d495e27e7688
Ripudaman Singh <rds0751@gmail.com>
2019-12-06 16:12:59 +0530

    msg

--- realestate/settings.py
+++ realestate/settings.py
@@ -80,7 +80,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware'
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
