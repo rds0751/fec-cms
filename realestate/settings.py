0a63c3cf13f95c244a688a6f02d24e458a8526e8
Ripudaman Singh <rds0751@gmail.com>
2019-12-08 12:07:45 +0530

    msg

--- realestate/settings.py
+++ realestate/settings.py
@@ -170,8 +170,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media Folder Settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
